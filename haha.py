ccb4349e70027a9d5269571567c421450d2ac9e3
Zhu Zhong <zhongzhu@ymail.com>
2014-12-14 21:03:02 +0800

    add haha.py

---
+++ haha.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from pyquery import PyQuery as q
+import requests
+import json
+
+url = u'http://w3school.com.cn/'
+
+r = requests.get(url)
+r.encoding = 'gb2312'
+# get the string: 领先的 Web 技术教程 - 全部免费
+s = q(r.text)('#w3 h2').text()
+
+with open('haha.json', 'w') as f:
+    json.dump([s], f)
