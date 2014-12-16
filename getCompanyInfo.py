3169710a27d9db9d63743206ad37584e73198dfd
Zhu Zhong <zhongzhu@ymail.com>
2014-12-16 20:23:27 +0800

    add some comments

--- getCompanyInfo.py
+++ getCompanyInfo.py
@@ -8,13 +8,10 @@ import itertools
 import json
 import time
 
+# get company name/intro/url
 def extractCompanyInfo(url, response):
     response.encoding = 'gb2312'
     q = pq(response.text)
-
-    # print(type(intro))
-    # print(intro)
-    # print(intro.encode('gb2312'))
 
     return {'name': q('#wrap #head .name h1').text(),
         'intro': q('.about p').contents()[0],
@@ -23,13 +20,13 @@ def extractCompanyInfo(url, response):
 if __name__ == "__main__":
     t1 = time.time()
 
-    # read the company url list 
+    # read the company urls 
     f = open('companyUrls.json', 'r')
     companyUrls = json.load(f)
     f.close()
 
-    #   
-    companyUrls = companyUrls[0:1]
+    # iterate all urls to get company name/intro/url  
+    companyUrls = companyUrls[0:1] # for test purpose, just get one company's info
     companyInfo = map(extractCompanyInfo, companyUrls,
         grequests.map((grequests.get(u) for u in companyUrls), size = 1))
 
